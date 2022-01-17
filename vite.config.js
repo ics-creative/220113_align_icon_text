90ea2f0d2362a2a5ea8795109aa25fa6d0da8e16
ics-furudate <furudate@ics-web.jp>
2022-01-17 15:08:13 +0900

    03-etcに例を追加、ディレクトリの移動、ファイルの分割

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ module.exports = defineConfig({
     outDir: "docs",
     rollupOptions: {
       input: {
-        top: resolve(__dirname, "index.html"),
-        layout: resolve(__dirname, "01-layout/index.html"),
-        animation: resolve(__dirname, "02-animation/index.html"),
-        etc: resolve(__dirname, "03-etc/index.html"),
+        top: resolve(__dirname, "./index.html"),
+        layout: resolve(__dirname, "./pages/01-layout/index.html"),
+        animation: resolve(__dirname, "./pages/02-animation/index.html"),
+        etc: resolve(__dirname, "./pages/03-etc/index.html"),
       },
     },
   },
